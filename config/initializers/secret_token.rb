a4bc65d12b6b79040e6a02dfbbba12a2a43263cb
Sonicrida <Sonicrida@live.com>
2012-10-02 17:39:03 -0400

    Project created. Initial db migrations done.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'e5889b795b3992dfcb7dc53004c1653b464f1a021cae224b2b07579816cb77fbe659a9d7083d54d18fc67c06c3925416eccde1a2bd5d655cf5ceb49347f12033'
